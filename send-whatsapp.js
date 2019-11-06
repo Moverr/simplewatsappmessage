0f059add9908cfa57faf9cad4a69d48674f5c62a
Muyinda Rogers <moverr@gmail.com>
2019-11-06 15:39:20 +0300

    :Debuging

--- send-whatsapp.js
+++ send-whatsapp.js
@@ -2,5 +2,6 @@ const client = request('twilio')();
 
 client.messages.create({
     from: 'whatsapp:+14155238886',
-    to: 'whatsapp:+14155238886'
-})
+    to: 'whatsapp:+256779820962',
+    body: 'Hello Testing API Message Sending'
+}).then(message => console.log(message.sid));
