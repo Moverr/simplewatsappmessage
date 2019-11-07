acb7ffa20dcf65fb6e722c054c3e274b6544a25e
Muyinda Rogers <moverr@gmail.com>
2019-11-07 12:41:44 +0300

    :Debuging:

--- send-whatsapp.js
+++ send-whatsapp.js
@@ -1,15 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
 const TWILIO_PUBLICKEY = process.env.TWILIO_PUBLICKEY;
 const TWILIO_SECRETKEY = process.env.TWILIO_SECRETKEY;
 
 const client = require('twilio')(TWILIO_PUBLICKEY, TWILIO_SECRETKEY);
 
-
+/*
 
 client.messages.create({
     from: 'whatsapp:+14155238886',
     to: 'whatsapp:+256779820962',
     body: 'Hello Testing API Message Sending'
 }).then(message => console.log(message.sid));
+
+
+*/
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 // to: 'whatsapp:+256779820962',
